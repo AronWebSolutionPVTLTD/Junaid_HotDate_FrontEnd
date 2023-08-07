d9e40f3c94612736955936cdf439645876fd8151
Vijay Tahkur <dev@aron123>
2023-08-07 16:29:28 +0530

    topG

---
+++ topG/src/Dashboard/MainDashboard/db_components/Pagination.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Pagination = ({
+  totalPosts,
+  postsPerPage,
+  setCurrentPage,
+  currentPage,
+}) => {
+  let pages = [];
+  console.log(totalPosts, postsPerPage);
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pages.push(i);
+  }
+  console.log(pages);
+  return (
+    <div className="grid gap-y-10 justify-items-center justify-center md:flex items-center md:justify-between my-10 md:my-16">
+      <div className="flex-1 flex justify-center">
+        {totalPosts / postsPerPage >= 1 ? (
+          <Link
+            className="primary_btn min-w-[200px] text-center h-16 inline-flex justify-center items-center"
+            onClick={() => {
+              setCurrentPage(currentPage + 1);
+              window.scrollTo(0, 0);
+            }}
+          >
+            Next Page
+          </Link>
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="flex items-center">
+        {pages.map((page, i) => (
+          <span
+            key={i}
+            className={`cursor-pointer text-sm w-8 h-8 flex items-center justify-center ${
+              currentPage === page ? "border border-orange" : ""
+            }`}
+            onClick={() => {
+              setCurrentPage(page);
+              window.scrollTo(0, 0);
+            }}
+          >
+            {page}
+          </span>
+        ))}
+        {/* <span className="cursor-pointer text-sm w-8 h-8 flex items-center justify-center">
+          1
+        </span>
+
+        <span className="cursor-pointer text-sm w-8 h-8 flex items-center justify-center">
+          3
+        </span> */}
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
---
+++ topG/src/LandingPage/components/HeroSection2.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HeroSection2 = () => {
+  return (
+    <section className="hero_section2 relative">
+      <img
+        src="landingPage/images/women-with-device.png"
+        className="w-full block object-cover inset-0 md:static"
+        alt=""
+      />
+      <div className="static md:absolute inset-0 flex items-center justify-center">
+        <div className="container pt-10 pb-20 md:pb-200px">
+          <div class="inner-block text-white flex flex-col justify-center xl:justify-start items-center lg:items-start text-center xl:text-start max-w-500 me-auto ms-auto lg:ms-0 lg:me-auto">
+            <p class="text-xl md:text-xl xl:text-35px leading-45 font-light">
+              Discover the ease and flexibility
+            </p>
+            <h2 class="text-2xl md:text-4xl xl:text-50px leading-75 font-bold">
+              of hooking up on any device!
+            </h2>
+            <p class="text-sm md:text-base xl:text-lg mb-4 leading-29 text-cetner lg:text-left">
+              Stay connected and never miss out on the fun with SWINXTER!
+              Experience the convenience of accessing all the features you enjoy
+              on your computer right from your smartphone or tablet. Browse
+              Swinxter® member profiles, send email messages, view cams, and
+              chat directly from your mobile device. Find hook-ups anytime,
+              anywhere – it's like having a party in your pocket! Join now to
+              bring the excitement with you on the go.
+            </p>
+            <Link class="primary_btn">Join for Free Now!</Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection2;
