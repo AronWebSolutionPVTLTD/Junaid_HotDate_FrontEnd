36f58cf630b26fde3444c9f968045c05a15aed32
Aronwebsolution <44797871+AronWebSolutionPVTLTD@users.noreply.github.com>
2023-10-27 18:47:40 +0530

    Update EventDetailPage.jsx

--- src/LandingPage/Pages/EventDetailPage.jsx
+++ src/LandingPage/Pages/EventDetailPage.jsx
@@ -289,7 +289,7 @@ set
                     >
                       Send Join Request
                     </button>
-                  )
+                  
                 ) : (
                   ""
                 )}
